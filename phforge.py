9c8dd22b799a632cffd84fcab5d670bb0a42332c
Antonino Ingargiola <tritemio@gmail.com>
2015-10-29 16:43:57 -0700

    First commit

---
+++ phforge.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+"""
+Create Photon-HDF5 from YAML and and a temporary HDF5 file.
+"""
+
+import sys
+import os
+from textwrap import dedent
+import yaml
+import h5py
+import phconvert as phc
+
+__version__ = 0.1
+
+
+def help():
+    """Print help"""
+    msg = """\
+    phforge {version} (phconvert {phc_version})
+
+    Create a Photon-HDF5 file (www.photon-hdf5.org) from metadata in
+    a YAML file and photon_data arrays in a (temporary) HDF5 file.
+
+    Usage:
+
+        phforge metadata_file hdf5_file output_file
+
+        metadata_file:
+            file name for the YAML file containing metadata
+
+        hdf5_arrays:
+            file name of the HDF5 file containing the photon_data arrays.
+
+        output_file:
+            file name for the Photon-HDF5 file to be created.
+    """.format(version=__version__, phc_version=phc.__version__)
+    print(dedent(msg))
+
+def error(msg):
+    print('ERROR: %s \n' % msg)
+    print('Type phforge with no arguments for help.\n')
+    sys.exit(1)
+
+def main():
+    print()
+    ## Check arguments
+    if len(sys.argv) == 1 or sys.argv[1] == '-h' or sys.argv[1] == '--help':
+        help()
+        sys.exit(0)
+    if len(sys.argv) != 4:
+        error('You need to pass 2 or 3 arguments.')
+
+    ## Check input files exists
+    metadata_fname, hdf5_fname, out_fname = sys.argv[1:]
+    if not os.path.isfile(metadata_fname):
+        error('Metadata file "%s" not found.')
+    if not os.path.isfile(hdf5_fname):
+        error('HDF5 file "%s" not found.')
+    if os.path.isfile(out_fname):
+        error('A file "%s" already exists.')
+
+    ## Load data
+    data = yaml.load(open(metadata_fname))
+    with h5py.File(hdf5_fname) as h5file:
+        for name in h5file:
+            data['photon_data'][name] = h5file[name][:]
+
+    ## Save Photon-HDF5 file
+    phc.hdf5.save_photon_hdf5(data, h5_fname=out_fname)
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+project_name = 'phforge'
+__version__ = 0.1
+
+long_description = """
+phforge
+=======
+
+This is a script to create `Photon-HDF5 <http://photon-hdf5.org/>`_ files
+from a YAML file contianing metadata and an HDF5 file containing
+the photon_data arrays.
+
+"""
+
+setup(
+    name = project_name,
+    version=__version__,
+    author = 'Antonino Ingargiola',
+    author_email = 'tritemio@gmail.com',
+    # url          = 'http://photon-hdf5.github.io/phconvert/',
+    # download_url = 'http://photon-hdf5.github.io/phconvert/',
+    install_requires = ['h5py', 'pyyaml', 'phconvert'],
+    license = 'MIT',
+    description = ("Create Photon-HDF5 files."),
+    long_description = long_description,
+    platforms = ('Windows', 'Linux', 'Mac OS X'),
+    classifiers=['Intended Audience :: Science/Research',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.4',
+                 'Topic :: Scientific/Engineering',
+                 ],
+    py_modules = ['phforge'],
+    entry_points={'console_scripts': ['phforge = phforge:main']},
+    keywords = ('single-molecule FRET smFRET biophysics file-format HDF5 '
+                'Photon-HDF5')
+)
